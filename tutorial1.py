0bd8cefdb954f22aa2e969e6bef06cb2e44d9cf9
LethalWarrior <albertfiraz@gmail.com>
2021-03-31 12:50:41 +0700

    Update tutorial1.py
    Added image rotation and image save codes

--- tutorial1.py
+++ tutorial1.py
@@ -4,6 +4,10 @@ import cv2
 img = cv2.imread('assets/lena.jpg', cv2.IMREAD_GRAYSCALE)
 # Resize the image to half the size
 img = cv2.resize(img, (0, 0), fx=0.5, fy=0.5)
+# Rotate the image
+img = cv2.rotate(img, cv2.cv2.ROTATE_90_COUNTERCLOCKWISE)
+
+cv2.imwrite('assets/rotated_lena_grayscale_resize_50.jpg', img)
 
 cv2.imshow('Lena Grayscale', img)
 # Wait indefinitely
